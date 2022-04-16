7fdc8ba6575472ed5917783c86072598bd438d54
sunami09 <sunamidasgupta@gmail.com>
2022-04-17 01:15:31 +0530

    Header Section - Complete

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="relative h-screen bg-gradient-to-b from-green-900/10 to-[#010511] lg:h-[140vh]">
+      <Head>
+        <title>Netflix | Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Model */}
+    </div>
+  )
+}
+
+export default Home
