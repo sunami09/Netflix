a279b712b59ee14f58c1e5afd5d08e5b0c4fcad7
sunami09 <sunamidasgupta@gmail.com>
2022-04-18 21:18:04 +0530

    Thumbnail Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0df8ce3d05900f6a"; }
+/******/ 		__webpack_require__.h = function() { return "ccb57437e213e21b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -6,26 +6,17 @@ interface Props {
 }
 
 function Thumbnail({movie} : Props) {
-    const links = ["esGtYJE3uoRuwA7o4ST2kGDhmeO.jpg", "vkojfC1xykW9XBCyDDCMYeHqLEf.jpg", "39EQTf94Lckdo8yzS1CdWQSgmen.jpg", "sqLNgWTMBhLBRY26SwwfWcSI5qq.jpg", "4rsomWxlqnHt3muGYK06auhOib6.jpg", "rk860hTM0kpu8oNhQkpnDv320rH.jpg", "mMtUybQ6hL24FXo0F3Z4j2KG7kZ.jpg"]
-    let p = Math.floor(Math.random() * links.length)
-
-    // console.log(movie.backdrop_path)
+    
   return (
       <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
-        if(movie.backdrop_path == null && movie.poster_path == null){
-              < Image
-                  src={`https://image.tmdb.org/t/p/w500/${links[p]}`}
-                  className="rounded-sm object-cover md:rounded"
-                  layout="fill"
-              />
-        }else{
+       
               <Image
                   src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
                       }`}
                   className="rounded-sm object-cover md:rounded"
                   layout="fill"
               />
-        }
+        
               
         
         
